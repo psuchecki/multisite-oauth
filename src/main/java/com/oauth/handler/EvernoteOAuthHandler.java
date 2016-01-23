5b28e860e1fbde8a30f2a846c0861d0412aca21b
psuchecki <piotr.jan.suchecki@gmail.com>
2016-01-23 21:35:04 +0800

    Saving notes and attached files in local directory

--- src/main/java/com/oauth/handler/EvernoteOAuthHandler.java
+++ src/main/java/com/oauth/handler/EvernoteOAuthHandler.java
@@ -9,6 +9,7 @@ import com.evernote.edam.notestore.NoteList;
 import com.evernote.edam.type.Note;
 import com.evernote.edam.type.NoteSortOrder;
 import com.evernote.edam.type.Notebook;
+import com.evernote.edam.type.Resource;
 import com.github.scribejava.apis.EvernoteApi;
 import com.github.scribejava.core.model.Token;
 import com.github.scribejava.core.model.Verifier;
@@ -23,10 +24,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 
 public class EvernoteOAuthHandler implements OAuthHandler {
@@ -75,6 +78,7 @@ public class EvernoteOAuthHandler implements OAuthHandler {
                     resp.getWriter().println(note);
                 }
             } catch (Exception e) {
+                e.printStackTrace();
                 resp.getWriter().println(e);
             }
         }
@@ -91,15 +95,32 @@ public class EvernoteOAuthHandler implements OAuthHandler {
             filter.setNotebookGuid(notebook.getGuid());
             filter.setOrder(NoteSortOrder.CREATED.getValue());
             filter.setAscending(true);
-            NoteList notes = noteStoreClient.findNotes(filter, 0, Integer.MAX_VALUE);
-            userNotes.addAll(notes.getNotes());
+            NoteList noteList = noteStoreClient.findNotes(filter, 0, Integer.MAX_VALUE);
+            List<Note> notes = noteList.getNotes();
+
             Path path = Paths.get(String.format("target/%s/%s.txt",APP_NAME, notebook.getGuid()));
             Files.createDirectories(path.getParent());
             Files.deleteIfExists(path);
             Files.createFile(path);
+
+            for (Note note : notes) {
+                Note noteWithContent = noteStoreClient.getNote(note.getGuid(), true, true, false, false);
+                List<Resource> resources = noteWithContent.getResources();
+                if (resources != null) {
+                    for (Resource resource : resources) {
+                        byte[] fileContent = resource.getData().getBody();
+                        String fileName = resource.getAttributes().getFileName();
+                        String filePath = String.format("target/%s/%s-%s", APP_NAME, note.getGuid(), fileName);
+                        com.google.common.io.Files.write(fileContent, new File(filePath));
+                    }
+                }
+                noteWithContent.setResources(Collections.emptyList());
+                userNotes.add(noteWithContent);
+            }
+
             try (BufferedWriter writer = Files.newBufferedWriter(path)) {
                 Gson gson = new Gson();
-                writer.write(gson.toJson(notes.getNotes()));
+                writer.write(gson.toJson(userNotes));
                 writer.close();
             }
         }
