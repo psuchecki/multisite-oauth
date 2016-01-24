8ff0731fdbe9e8f379036b55ac89db344e4a814a
psuchecki <piotr.jan.suchecki@gmail.com>
2016-01-25 02:27:24 +0800

    Small fix to not overwrite files if two users have same files

--- src/main/java/com/oauth/client/DropboxFileDownloader.java
+++ src/main/java/com/oauth/client/DropboxFileDownloader.java
@@ -6,6 +6,8 @@ import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.github.scribejava.core.model.Token;
 import com.google.common.collect.Lists;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.oauth.handler.DropboxOAuthHandler;
 
 import java.io.FileOutputStream;
@@ -22,29 +24,32 @@ public class DropboxFileDownloader implements FileDownloaderApi {
     @Override
     public Token downloadUserFiles(String rawResponse) throws Exception {
         Token accessToken = DROP_BOX_PROVIDER.getAccessTokenExtractor().extract(rawResponse);
-        List<String> downloadedFiles = Lists.newArrayList();
         DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0", Locale.getDefault().toString());
         DbxClient client = new DbxClient(config, accessToken.getToken());
-        visitFolder(client, "/", downloadedFiles);
+
+        List<String> downloadedFiles = Lists.newArrayList();
+        JsonObject rawResponseJson = new JsonParser().parse(accessToken.getRawResponse()).getAsJsonObject();
+        String clientUid = rawResponseJson.get("uid").getAsString(); //needed to not overwrite if clients have same files
+        visitFolder(client, "/", clientUid, downloadedFiles);
 
         return accessToken;
     }
 
-    private void visitFolder(DbxClient client, String folderPath, List<String> downloadedFiles)
+    private void visitFolder(DbxClient client, String folderPath, String clientUid, List<String> downloadedFiles)
             throws DbxException, IOException {
         DbxEntry.WithChildren listing = client.getMetadataWithChildren(folderPath);
         for (DbxEntry child : listing.children) {
             if (child.isFile()) {
-                downloadFile(client, child, downloadedFiles);
+                downloadFile(client, child, clientUid, downloadedFiles);
             } else if (child.isFolder()) {
-                visitFolder(client, child.path, downloadedFiles);
+                visitFolder(client, child.path, clientUid, downloadedFiles);
             }
         }
     }
 
-    private void downloadFile(DbxClient client, DbxEntry file, List<String> downloadedFiles)
+    private void downloadFile(DbxClient client, DbxEntry file, String clientUid, List<String> downloadedFiles)
             throws IOException, DbxException {
-        String filePath = String.format("target/%s%s", DropboxOAuthHandler.APP_NAME, file.path);
+        String filePath = String.format("target/%s/%s%s", DropboxOAuthHandler.APP_NAME, clientUid, file.path);
         Path path = Paths.get(filePath);
         Files.createDirectories(path.getParent());
         Files.deleteIfExists(path);
